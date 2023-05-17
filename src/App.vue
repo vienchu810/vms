e76c25aa9fcbfddce69412bc054e0de205c9012b
vienchu810 <75822400+vienchu810@users.noreply.github.com>
2023-05-17 16:28:31 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div class="container m-auto" style="background: rgb(84 84 84); color: white;" >
+    <div class="row">
+      <div class="col-12 col-md-4"> 
+         <div class="d-flex  align-items-center "> 
+     <router-link  to="/"><p class="color-white mb-0 "> Home</p></router-link> |
+     <router-link to="/about"><p class="color-white mb-0"> Feed</p></router-link>
+  </div>
+      </div>
+      <div class="col-12 col-md-3"> 
+         <div class="d-flex"><input name="search" id="search" placeholder="nhập thông tin tìm kiếm"> 
+      <button>  <i class="fa fa-search"> </i></button>
+    </div>
+        </div>
+        <div class="col-12 col-md-5"> 
+         <div class="d-flex"><input name="search" id="search" > 
+      <button>  <i class="fa fa-search"> </i></button>
+    </div>
+        </div>
+    </div>
+   
+   
+  </div> -->
+  <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>V<em>sound</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+
+              
+              <li class="nav-item active">
+                <a class="nav-link" href="/">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="/about">favoris</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/about">Category</a>
+              </li>
+              <li class="nav-item mt-2 ">
+                <input placeholder="Search" id="searchSound" name="searchSound">
+                <button> <i class="fa fa-search"> </i></button>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">For Producer </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Upload</a>
+              </li>
+              <li class="nav-item d-flex">
+                <img style="width: 30px; height:30px;  border-radius: 50px;" class="mt-2"  src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/345256734_278921197796034_4130124785679367968_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=tLVTwABH6iYAX8n5fkT&_nc_ht=scontent.fhan17-1.fna&oh=00_AfDsRJMPMk03qolFEhfyXrlcLcdeRhMOo9MEXK2F6Euo1w&oe=64634837">
+                <a class="nav-link color-white"> Profile</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+ 
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +73,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,166 @@
+<template>
+ 
+ <div class="main-banner" id="top">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="left-content">
+                <div class="thumb">
+                  <div class="inner-content">
+                    <h4>Top hit 100</h4>
+
+                    <div class="main-border-button">
+                      <a href="#">Xem ngay</a>
+                    </div>
+                  </div>
+                  <img  class="mt-2" style="width: 100%;" src="https://focusevent.vn/wp-content/uploads/2020/12/photo-11.jpg">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+
+              <div class="right-content">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="right-first-image">
+                    <div class="thumb">
+                  <div class="inner-content">
+                   <h5> <strong style="color: white;">Đưa âm nhạc của bạn đến gần hơn với mọi người</strong></h5>
+
+                    <div class="main-border-button mt-2">
+                      <a href="#">Tại đây</a>
+                    </div>
+                  </div>
+                  <img  class="mt-2"  src="https://ss-images.saostar.vn/pc/1663294672127/saostar-3xewuuj7qxn73ehk.jpg">
+                </div>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-12">
+                    <div class="right-first-image">
+                    <div class="thumb">
+                  <div class="inner-content">
+                    <h4>Top thịnh hành</h4>
+
+                    <div class="main-border-button">
+                      <a href="#">Xem ngay</a>
+                    </div>
+                  </div>
+                  <img  class="mt-2"  src="https://vn.blog.kkday.com/wp-content/uploads/danny-howe-bn-D2bCvpik-unsplash.jpg">
+                </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              
+              <div class="right-content">
+                <div class="row" v-if="categories && categories.length">
+                  <div class="col-lg-12" v-for="category in categories" :key="category.id">
+                  <div class="inner-content " style="background: #000;">
+                    <div class="main-border-button mt-2 " :style="getBackgroundStyle(category.linkBackGround) " style="position: relative; height: 96px;  background-size: cover;  background-position: center center; " >
+                      <a href="#" class="mt-4"  style="z-index: 2;">{{category.nameCategory}}</a>
+                    </div>
+                  </div>
+                
+                </div>
+                <div v-if="error">{{ error }}</div>
+                <!-- <div class="col-lg-12">
+                  <div class="inner-content " style="background: #000;">
+                    <div class="main-border-button mt-2 " style="position: relative; height: 96px; background-size: cover;  background-position: center center;  background-image: url(https://toigingiuvedep.vn/wp-content/uploads/2021/10/hinh-anh-chill-dep-buon-cham-toi-trai-tim-bao-nguoi-2.jpg);">
+                   
+                      <a href="#" class="mt-4"  style="z-index: 2;">LO-FI</a>
+                    </div>
+                  </div>
+                
+                </div>
+                <div class="col-lg-12">
+                  <div class="inner-content " style="background: #000;">
+                    <div class="main-border-button mt-2 " style="position: relative; height: 96px; background-size: cover;  background-position: center center;  background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNZLhDJfitlsZGSLSQuCpzK2i6gMMVHXUhK8ZmePMNTgGR7I50XGa3sq7yv7F8Uc233Zs&usqp=CAU);">
+                   
+                      <a href="#" class="mt-4"  style="z-index: 2;">Rap Hip Hop</a>
+                    </div>
+                  </div>
+                
+                </div>
+                <div class="col-lg-12">
+                  <div class="inner-content " style="background: #000;">
+                    <div class="main-border-button mt-2 " style="position: relative; height: 96px; background-size: cover;  background-position: center center;  background-image: url(https://cdn.tgdd.vn/Files/2021/08/23/1377229/nhac-ballad-la-gi-nguon-goc-va-dac-diem-noi-bat-c-2.jpg);">
+                   
+                      <a href="#" class="mt-4"  style="z-index: 2;">Ballad</a>
+                    </div>
+                  </div>
+                
+                </div>
+                <div class="col-lg-12">
+                  <div class="inner-content " style="background: #000;">
+                    <div class="main-border-button mt-2 " style="position: relative; height: 96px; background-size: cover;  background-position: center center;  background-image: url(https://www.taylorguitars.com/sites/default/files/styles/hero_rotator_image/public/images/2022-09/HGG-2-2250x762_0.jpg?itok=TOLcCMk3);">
+                   
+                      <a href="#" class="mt-4"  style="z-index: 2;">Acoustic Guitars</a>
+                    </div>
+                  </div>
+                
+                </div>
+                    
+                <div class="col-lg-12">
+                  <div class="inner-content " style="background: #000;">
+                    <div class="main-border-button mt-2 " style="position: relative; height: 96px; background-size: cover;  background-position: center center;  background-image: url(https://media.graphassets.com/XrgacrQwKVeqJ1HyE8wg);">
+                   
+                      <a href="#" class="mt-4"   style="z-index: 2;">BEAT</a>
+                    </div>
+                  </div>
+                
+                </div> -->
+
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      categories: []
+    };
+  },
+  created() {
+    this.getCate();
+  },
+  
+  methods: {
+    getCate() {
+      axios.get('http://localhost:9090/api/getCate')
+        .then(response => {
+          console.log("response.data "+ response.data.data)
+          this.categories = response.data.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    getBackgroundStyle(url) {
+      return `background-image: url(${url})`;
+    }
+  }
+};
+</script>
+<!-- <script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script> -->
